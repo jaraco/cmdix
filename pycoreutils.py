fbb31552c9990f668c6c9d007b23276afc66270e
Hans van Leeuwen <hanz@hanz.nl>
2009-09-11 00:36:36 +0200

    Major overhaul: Removed symlinks and moved pycoreutils

--- pycoreutils.py
+++ pycoreutils.py
@@ -3,7 +3,7 @@
 from __future__ import with_statement 
 import hashlib, logging, optparse, os, platform, random, shutil, subprocess, sys, time
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __license__ = '''
 Copyright (c) 2009 Hans van Leeuwen
 
--- setup.py
+++ setup.py
@@ -2,23 +2,19 @@
 from distutils.core import setup
 import pycoreutils
 
-
 setup(
     name = "pycoreutils",
     version = pycoreutils.__version__,
-    description = "Python version of GNU coreutils"
+    description = "Python port of GNU coreutils",
     classifiers = [
         "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
     ],
     license = "MIT",
     url = "http://hanz.nl",
     author = "Hans van Leeuwen",
     author_email = "hanz@hanz.nl",
-    scripts = [
-        "pycoreutils",
-    ],
-    packages = [
-        "pycoreutils",
-    ],
+    py_modules = ["pycoreutils"],
 )
 
