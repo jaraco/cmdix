793e22f37d7ae1600eca30ca9cd717a4d146fd38
Hans van Leeuwen <hanz@hanz.nl>
2010-12-22 11:29:22 +0100

    Removed unneeded license

--- sh.py
+++ sh.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Copyright (c) 2010 Hans van Leeuwen.
+# Release under the MIT license.
+# See LICENSE for details.
+
 import cmd
 import os
 import platform
@@ -8,27 +12,6 @@ import sys
 
 import pycoreutils
 
-
-__version__ = '0.0.1'
-__license__ = '''Copyright (c) 2010 Hans van Leeuwen
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
-of the Software, and to permit persons to whom the Software is furnished to do
-so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-'''
 
 class PyCoreutilsShell(cmd.Cmd):
 
@@ -70,4 +53,4 @@ class PyCoreutilsShell(cmd.Cmd):
 
 
 if __name__ == '__main__':
-    PyCoreutilsShell().cmdloop('PyCoreutils Shell version %s' % __version__)
+    PyCoreutilsShell().cmdloop('PyCoreutils Shell version %s' % pycoreutils.__version__)
