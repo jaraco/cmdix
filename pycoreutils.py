71ac9c9b7a6ac2d42da4a3c23818a27dfb91e453
Hans van Leeuwen <hanz@hanz.nl>
2010-12-30 18:42:38 +0100

    Refactored 'uname'

--- pycoreutils.py
+++ pycoreutils.py
@@ -1003,42 +1003,46 @@ def uname(argstr):
     p.add_option("-m", "--machine", action="store_true", dest="machine",
             help="print the machine hardware name")
     p.add_option("-p", "--processor", action="store_true", dest="processor",
-            help="print the processor type or \"unknown\"")
+            help='print the processor type or "unknown"')
     p.add_option("-i", "--hardware-platform", action="store_true", dest="hardwareplatform",
             help="print the hardware platform or \"unknown\"")
     p.add_option("-o", "--operating-system", action="store_true", dest="operatingsystem",
             help="print the operating system")
+    p.add_option("-A", "--architecture", action="store_true", dest="architecture",
+            help="print the systems architecture")
     (opts, args) = p.parse_args(argstr.split())
 
-    x = None
+    output = []
 
     if opts.kernelname or opts.all:
-        x = True
-        print(platform.system(), end='')
-    if opts.nodename or opts.all:
-        x = True
-        print(platform.node(), end='')
-    if opts.kernelrelease or opts.all:
-        x = True
-        print(platform.release(), end='')
-    if opts.kernelversion or opts.all:
-        x = True
-        print(platform.version(), end='')
-    if opts.machine or opts.all:
-        x = True
-        print(platform.machine(), end='')
-    if opts.processor or opts.all:
-        x = True
-        print(platform.processor(), end='')
-    if opts.hardwareplatform or opts.all:
-        x = True
-        print(platform.architecture()[0], end='')
-    if opts.operatingsystem or opts.all:
-        x = True
-        print(platform.system(), end='')
+        output.append(platform.system())
 
-    if not x:
-        print(platform.system(), end='')
+    if opts.nodename or opts.all:
+        output.append(platform.node())
+
+    if opts.kernelrelease or opts.all:
+        output.append(platform.release())
+
+    if opts.kernelversion or opts.all:
+        output.append(platform.version())
+
+    if opts.machine or opts.all:
+        output.append(platform.machine())
+
+    if opts.processor:
+        output.append(platform.processor())
+
+    if opts.hardwareplatform:
+        # Didn't find a way to get this
+        output.append('unknown')
+
+    if opts.architecture:
+        output.append(" ".join(platform.architecture()))
+
+    if opts.operatingsystem or opts.all or output == []:
+        output.append(platform.system())
+
+    print(" ".join(output))
 
 
 @addcmd
