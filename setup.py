dd855244ca97d5c5d58bb46d8f69e0844a5f8a97
Hans van Leeuwen <hanz@hanz.nl>
2011-01-04 15:47:28 +0100

    Use README.txt as long_description

--- setup.py
+++ setup.py
@@ -2,17 +2,26 @@
 from distutils.core import setup
 import pycoreutils
 
+# Use README.txt + a list of available commands as long_description
+long_description = ''.join(open("README.txt").readlines()) + '''
+
+List of available commands
+--------------------------
+
+- ''' + "\n- ".join(pycoreutils.listcommands())
+
 setup(
     name = "pycoreutils",
     version = pycoreutils.__version__,
-    description = "Python port of GNU coreutils",
+    description = "Coreutils in Pure Python",
+    long_description = long_description,
     classifiers = [
         "Development Status :: 3 - Alpha",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
     ],
     license = "MIT",
-    url = "http://hanz.nl",
+    url = "https://launchpad.net/pycoreutils",
     author = "Hans van Leeuwen",
     author_email = "hanz@hanz.nl",
     packages = ["pycoreutils", "pycoreutils.tests"],
