4c3cbb976dce4cfbe3e35284a83726bdb21a720e
Hans van Leeuwen <hanz@hanz.nl>
2011-01-08 23:06:12 +0100

    Added very basic 'tar'

--- pycoreutils/__init__.py
+++ pycoreutils/__init__.py
@@ -53,6 +53,7 @@ import smtplib
 import socket
 import stat
 import subprocess
+import tarfile
 import tempfile
 import textwrap
 import time
@@ -1191,6 +1192,78 @@ def tail(argstr):
             f.seek(where)
         else:
             yield line
+
+
+@addcommand
+def tar(argstr):
+    p = parseoptions()
+    p.description = "saves many files together into a single tape or disk " +\
+                   "archive, and can restore individual files from the archive"
+    p.usage = '%prog -x [OPTION]...\n' + \
+       '       %prog -t [OPTION]...\n' + \
+       '       %prog -c [OPTION]... TARFILE SOURCE...\n'
+    p.epilog = "Files that end with '.bz2' or '.gz' are decompressed " +\
+               "automatically."
+    p.add_option("-c", "--create", action="store_true", dest="create",
+            help="create zipfile from source.")
+    p.add_option("-t", "--list", action="store_true", dest="list",
+            help="list files in zipfile.")
+    p.add_option("-x", "--extract", action="store_true", dest="extract",
+            help="extract tarfile into current directory.")
+    p.add_option("-j", "--bzip2", action="store_true", dest="bzip2",
+            help="(de)compress using bzip2")
+    p.add_option("-f", "--file", dest="archive",
+            help="use archive file or device ARCHIVE")
+    p.add_option("-z", "--gzip", action="store_true", dest="gzip",
+            help="(de)compress using gzip")
+    (opts, args) = p.parse_args(argstr.split())
+
+    if bool(opts.list) + bool(opts.create) + bool(opts.extract) > 1:
+        raise StdErrException("You can only use one of '-c', '-t' or 'x'")
+
+    if opts.extract or opts.list:
+        if opts.archive:
+            infile = open(opts.archive, 'rb')
+        else:
+            infile = sys.stdin
+        try:
+            tar = tarfile.open(fileobj=infile)
+        except tarfile.TarError as err:
+            raise StdErrException("Could not parse file " +\
+                "{0}. Are you sure it is a tar-archive?".format(infile.name))
+
+    if opts.extract:
+        tar.extractall()
+        tar.close()
+
+    elif opts.list:
+        for tarinfo in tar:
+            name = tarinfo.name
+            if tarinfo.isdir():
+                    name += '/'
+            yield name + '\n'
+        tar.close()
+
+    elif opts.create:
+        # Set outfile
+        if opts.archive:
+            outfile = open(opts.archive, 'wb')
+        else:
+            outfile = sys.stout
+
+        # Set mode
+        if opts.gzip:
+            mode = 'w:gz'
+        elif opts.bzip2:
+            mode = 'w:bz2'
+        else:
+            mode = 'w'
+        tar = tarfile.open(fileobj=outfile, mode=mode)
+        for arg in args:
+            tar.add(arg)
+        tar.close()
+    else:
+        raise StdErrException("Either '-c', '-t' or '-x' should be specified")
 
 
 @addcommand
---
+++ pycoreutils/tests/test_tar.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2009, 2010, 2011 Hans van Leeuwen.
+# Release under the MIT license.
+# See LICENSE.txt for details.
+
+from __future__ import unicode_literals
+
+import os
+import tarfile
+import unittest
+
+from pycoreutils import StdErrException
+from pycoreutils.tests import BaseTestCase
+
+
+class TestCase(BaseTestCase):
+    def test_notarfile(self):
+        self.createfile('foo', 100)
+        self.assertRaises(
+            StdErrException,
+            self.runcommandline,
+            'tar -tf foo'
+        )
+
+    def test_tar(self):
+        self.setup_filesystem()
+        good = ['dir1/',
+                'dir1/dir1-1/',
+                'dir1/dir1-1/dir1-1-1/',
+                'dir1/dir1-1/dir1-1-1/file1-1-1-1',
+                'dir1/dir1-1/dir1-1-2/',
+                'dir1/dir1-2/',
+                'dir1/dir1-2/dir1-2-1/',
+                'dir1/dir1-2/file1-2-1',
+                'dir1/dir1-2/file1-2-2',
+                'dir1/dir1-3/',
+                'dir1/file1-1',
+                'dir1/file1-2',
+                'dir2/',
+                'dir2/dir2-1/',
+                'dir2/dir2-2/',
+                'dir2/dir2-2/file2-2-1',
+                'file1']
+
+        for archive in ('foo.tar', 'foo.tar.bz2', 'foo.tar.gz'):
+            # Create an archive
+            self.assertEqual(self.runcommandline(
+                    'tar -cf {0} dir1 dir2 file1'.format(archive)), '')
+            l = []
+            for tarinfo in tarfile.open(archive):
+                name = tarinfo.name
+                if tarinfo.isdir():
+                    name += '/'
+                l.append(name)
+            l.sort()
+            self.assertEqual(l, good)
+
+            # List the archive
+            x = self.runcommandline(
+                    'tar -tf {0}'.format(archive)).split()
+            x.sort()
+            self.assertEqual(x, good)
+
+            # Extract the archive
+            """
+            d = os.getcwd()
+            os.mkdir('extractdir')
+            os.chdir('extractdir')
+            x = self.runcommandline(
+                    'tar -xf {0}'.format(os.path.join(d, archive))).split()
+            self.assertEqual(x, good)
+            """
+
+if __name__ == '__main__':
+    unittest.main()
