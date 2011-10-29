1f12fcc8f08bed7bdee9a23fc8fd927994507c14
Hans van Leeuwen <hanz@hanz.nl>
2011-10-29 15:48:50 +0200

    Added buildroot 2011.08

---
+++ buildroot/package/x11r7/xlib_libXau/xlib_libXau.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# xlib_libXau -- X.Org Xau library
+#
+################################################################################
+
+XLIB_LIBXAU_VERSION = 1.0.5
+XLIB_LIBXAU_SOURCE = libXau-$(XLIB_LIBXAU_VERSION).tar.bz2
+XLIB_LIBXAU_SITE = http://xorg.freedesktop.org/releases/individual/lib
+XLIB_LIBXAU_AUTORECONF = NO
+XLIB_LIBXAU_INSTALL_STAGING = YES
+XLIB_LIBXAU_DEPENDENCIES = xutil_util-macros xproto_xproto
+
+HOST_XLIB_LIBXAU_DEPENDENCIES = host-xutil_util-macros host-xproto_xproto
+
+$(eval $(call AUTOTARGETS,package/x11r7,xlib_libXau))
+$(eval $(call AUTOTARGETS,package/x11r7,xlib_libXau,host))
