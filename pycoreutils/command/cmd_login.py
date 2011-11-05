a957084996009ce0729069c6dc5d72bfdae71d86
Hans van Leeuwen <>
2011-11-05 23:16:25 +0100

    Do runcommandline('sh') if no shell is 'sh'

--- pycoreutils/command/cmd_login.py
+++ pycoreutils/command/cmd_login.py
@@ -11,6 +11,7 @@ import os
 import pwd
 import spwd
 import subprocess
+from multiprocessing import Process
 
 
 @pycoreutils.onlyunix
@@ -22,43 +23,51 @@ def login(p):
 
 
 def func(args):
-    # Get username
-    if args.username:
-        username = args.username
-    else:
-        username = raw_input("Username: ")
+    while True:
+        try:
+            # Get username
+            if args.username:
+                username = args.username
+            else:
+                username = raw_input("Username: ")
 
-    # Get password
-    password = getpass.getpass("Password: ")
+            # Get password
+            password = getpass.getpass("Password: ")
 
-    # Authenticate
-    try:
-        # Get entry from /etc/shadow
-        hashed_password = spwd.getspnam(username).sp_pwd
-    except KeyError:
-        print("Invalid username or password\n")
-        func(args)
-    else:
-        if check_password(hashed_password, password):
-            # Get entry from /etc/passwd
-            pw = pwd.getpwnam(username)
+            # Authenticate
+            try:
+                # Get entry from /etc/shadow
+                hashed_password = spwd.getspnam(username).sp_pwd
+            except KeyError:
+                print("Invalid username or password\n")
+                pass
+            else:
+                if check_password(hashed_password, password):
+                    # Get entry from /etc/passwd
+                    pw = pwd.getpwnam(username)
 
-            # Set UID of user
-            os.setuid(pw.pw_uid)
+                    # Set UID of user
+                    os.setuid(pw.pw_uid)
 
-            # Change to homedir
-            os.chdir(pw.pw_dir)
+                    # Change to homedir
+                    os.chdir(pw.pw_dir)
 
-            # Set enviornment
-            os.environ['USER'] = username
-            os.environ['LOGNAME'] = username
-            os.environ['HOME'] = pw.pw_dir
-            os.environ['SHELL'] = pw.pw_shell
+                    # Set enviornment
+                    os.environ['USER'] = username
+                    os.environ['LOGNAME'] = username
+                    os.environ['HOME'] = pw.pw_dir
+                    os.environ['SHELL'] = pw.pw_shell
 
-            # Start user shell
-            subprocess.call([pw.pw_shell])
-        else:
-            print("Invalid username or password\n")
+                    # Start user shell
+                    if pw.pw_shell == 'sh':
+                        subprocess.call([pw.pw_shell])
+                    else:
+                        p = Process(target=pycoreutils.run, args=[['sh']])
+                        p.start()
+                else:
+                    print("Invalid username or password\n")
+        except BaseException:
+            print()
 
 
 def check_password(hashed_password, password):
