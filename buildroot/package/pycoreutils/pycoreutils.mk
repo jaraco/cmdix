0d954ea4829fb66d3827cbfce9939c7dd8942feb
Hans van Leeuwen <>
2011-11-15 23:37:21 +0100

    Updated buildroot, removed pyos skeleton

--- buildroot/package/pycoreutils/pycoreutils.mk
+++ buildroot/package/pycoreutils/pycoreutils.mk
@@ -8,10 +8,13 @@ define PYCOREUTILS_BUILD_CMDS
 endef
 
 define PYCOREUTILS_INSTALL_TARGET_CMDS
+        rm -f $(addprefix $(TARGET_DIR)/usr/bin/,bzip2 bunzip2 bzcat)
 	(cd $(@D) && $(HOST_DIR)/usr/bin/python setup.py install --prefix=$(TARGET_DIR)/usr)
 	(cd $(TARGET_DIR) && ln -fs usr/bin/pycoreutils init)
 	(cd $(TARGET_DIR)/sbin && ln -fs ../usr/bin/pycoreutils init)
-        $(HOST_DIR)/usr/bin/python $(TARGET_DIR)/usr/bin/pycoreutils --createlinks $(TARGET_DIR)/usr/bin
+	(cd $(TARGET_DIR)/bin && ln -fs ../usr/bin/pycoreutils mount)
+	(cd $(TARGET_DIR)/bin && ln -fs ../usr/bin/pycoreutils sh)
+        PYTHONPATH=$(TARGET_DIR)/usr/lib/python2.7/site-packages $(HOST_DIR)/usr/bin/python $(TARGET_DIR)/usr/bin/pycoreutils --createlinks $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(call GENTARGETS,package,pycoreutils))
