41feb11f7473abfb03f5ca7fffb522ed16de9ef1
Hans van Leeuwen <>
2011-11-15 23:09:22 +0100

    Updated mount

--- pycoreutils/command/cmd_mount.py
+++ pycoreutils/command/cmd_mount.py
@@ -6,7 +6,10 @@
 from __future__ import print_function, unicode_literals
 import pycoreutils
 import ctypes
+import ctypes.util
 import sys
+
+# TODO: mount options
 
 
 @pycoreutils.onlyunix
@@ -25,40 +28,54 @@ def parseargs(p):
     p.add_argument('SOURCE', nargs='?')
     p.add_argument('DEST', nargs='?')
     p.add_argument("-a", "--all", action="store_true",
-            help="ignore, for compatibility with other versions")
+            help="Mount all filesystems mentioned in fstab")
     #p.add_argument("-o", "--options", default=0,
             #help="print only the effective group ID")
     p.add_argument("-t", "--types", default="ext2",
             help="Filesystem type. Supported types: " +\
                  ", ".join(available_filesystems))
+    p.add_argument("-v", "--verbose", action="store_true",
+            help="Output debugging information")
     return p
 
 
 def func(args):
     if args.SOURCE and args.DEST:
-        _mount(args.SOURCE, args.DEST, args.types)
-    elif args.SOURCE:
+        mount_c(args.SOURCE, args.DEST, args.types, args.verbose)
+    elif args.SOURCE or args.all:
+        # Read fstab
         try:
             with open('/etc/fstab') as fd:
                 lines = fd.readlines()
         except IOError:
             print("Error: Couldn't read /etc/ftab", file=sys.stderr)
             return
+
+        # Mount filesystem
+        mounted_something = False
         for line in lines:
+            line = line.partition('#')[0]  # Remove comments
             source, dest, fstype, options, freq, passno = line.split()
-            if source == args.SOURCE:
-                return _mount(source, dest, fstype)
-            elif dest == args.SOURCE:
-                return _mount(source, dest, fstype)
-        print(args.SOURCE + " not found in /etc/fstab", file=sys.stderr)
+            if source == args.SOURCE or dest == args.SOURCE or args.all:
+                mount_c(dest, dest, fstype, args.verbose)
+                mounted_something = True
+        if not mounted_something:
+            print(args.SOURCE + " not found in /etc/fstab", file=sys.stderr)
     else:
+        # Display currently mounted filesystems
         try:
             print(open('/etc/mtab').read().strip())
         except IOError:
             print("Error: Couldn't read /etc/mtab", file=sys.stderr)
 
 
-def _mount(source, dest, fstype, options=0, data=''):
+def mount_c(source, dest, fstype, options=0, data='', verbose=False):
+    '''
+    Frontend to libc mount
+    '''
+    if verbose:
+        print("Trying to mount {0} on {1} as type {2}".format(source, dest,
+                                                                       fstype))
     libc = ctypes.CDLL(ctypes.util.find_library('c'))
     res = libc.mount(str(source), str(dest), str(fstype), options, str(data))
     if res < 0:
