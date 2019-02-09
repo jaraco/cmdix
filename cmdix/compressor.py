0c8535dcebaedbe73321088c4445a9859de56335
Jason R. Coombs <jaraco@jaraco.com>
2019-02-09 17:50:17 -0500

    Feed the hobgoblins (delint).

--- cmdix/compressor.py
+++ cmdix/compressor.py
@@ -18,31 +18,43 @@ def compressor(p, comptype='gzip', decompress=False):
     p.set_defaults(func=compressorfunc, comptype=comptype,
                    decompress=decompress)
     p.add_argument('FILE', nargs='*')
-    p.add_argument("-c", "--stdout", "--as-stdout", action="store_true",
-            dest="stdout",
-            help="write on standard output, keep original files unchanged")
-    p.add_argument("-C", "--compresslevel", dest="compresslevel", type=int,
-            default=6, help="set file mode (as in chmod), not a=rwx - umask")
-    p.add_argument("-d", "--decompress", action="store_true",
-            dest="decompress", help="decompress")
-    p.add_argument("-1", "--fast", action="store_const", dest="compresslevel",
-            const=1, help="Use the fastest type of compression")
-    p.add_argument("-2", action="store_const", dest="compresslevel", const=2,
-            help="Use compression level 2")
-    p.add_argument("-3", action="store_const", dest="compresslevel", const=3,
-            help="Use compression level 3")
-    p.add_argument("-4", action="store_const", dest="compresslevel", const=4,
-            help="Use compression level 4")
-    p.add_argument("-5", action="store_const", dest="compresslevel", const=5,
-            help="Use compression level 5")
-    p.add_argument("-6", action="store_const", dest="compresslevel", const=6,
-            help="Use compression level 6")
-    p.add_argument("-7", action="store_const", dest="compresslevel", const=7,
-            help="Use compression level 7")
-    p.add_argument("-8", action="store_const", dest="compresslevel", const=8,
-            help="Use compression level 8")
-    p.add_argument("-9", "--best", action="store_const", dest="compresslevel",
-            const=9, help="Use the best type of compression")
+    p.add_argument(
+        "-c", "--stdout", "--as-stdout", action="store_true",
+        dest="stdout",
+        help="write on standard output, keep original files unchanged")
+    p.add_argument(
+        "-C", "--compresslevel", dest="compresslevel", type=int,
+        default=6, help="set file mode (as in chmod), not a=rwx - umask")
+    p.add_argument(
+        "-d", "--decompress", action="store_true",
+        dest="decompress", help="decompress")
+    p.add_argument(
+        "-1", "--fast", action="store_const", dest="compresslevel",
+        const=1, help="Use the fastest type of compression")
+    p.add_argument(
+        "-2", action="store_const", dest="compresslevel", const=2,
+        help="Use compression level 2")
+    p.add_argument(
+        "-3", action="store_const", dest="compresslevel", const=3,
+        help="Use compression level 3")
+    p.add_argument(
+        "-4", action="store_const", dest="compresslevel", const=4,
+        help="Use compression level 4")
+    p.add_argument(
+        "-5", action="store_const", dest="compresslevel", const=5,
+        help="Use compression level 5")
+    p.add_argument(
+        "-6", action="store_const", dest="compresslevel", const=6,
+        help="Use compression level 6")
+    p.add_argument(
+        "-7", action="store_const", dest="compresslevel", const=7,
+        help="Use compression level 7")
+    p.add_argument(
+        "-8", action="store_const", dest="compresslevel", const=8,
+        help="Use compression level 8")
+    p.add_argument(
+        "-9", "--best", action="store_const", dest="compresslevel",
+        const=9, help="Use the best type of compression")
     return p
 
 
@@ -70,8 +82,9 @@ def compressorfunc(args):
             else:
                 unzippath = infile.rstrip(suffix)
                 if os.path.exists(unzippath):
-                    q = input("{0}: {1} ".format(args.prog, unzippath) + \
-                         "already exists; do you wish to overwrite (y or n)? ")
+                    q = input(
+                        "{0}: {1} ".format(args.prog, unzippath) +
+                        "already exists; do you wish to overwrite (y or n)? ")
                     if q.upper() != 'Y':
                         StdOutException("not overwritten", 2)
 
@@ -84,8 +97,9 @@ def compressorfunc(args):
                 outfile = sys.stdout
             else:
                 if os.path.exists(zippath):
-                    q = input("{0}: {1} ".format(args.prog, zippath) + \
-                         "already exists; do you wish to overwrite (y or n)? ")
+                    q = input(
+                        "{0}: {1} ".format(args.prog, zippath) +
+                        "already exists; do you wish to overwrite (y or n)? ")
                     if q.upper() != 'Y':
                         StdErrException("not overwritten", 2)
 
