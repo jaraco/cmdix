f689df90df4be146bb852f58e73e9e12aa1643c6
Jason R. Coombs <jaraco@jaraco.com>
2019-02-09 22:25:54 -0500

    Monkeypatch the os module, but avoid leaving the patch to break other things later.

--- cmdix/test/test_onlyunix.py
+++ cmdix/test/test_onlyunix.py
@@ -4,15 +4,22 @@ from . import BaseTestCase
 import os
 import unittest
 
+import pytest
+
 import cmdix
 from .. import exception
 
 
+@pytest.fixture
+def pretend_windows(monkeypatch):
+    monkeypatch.setattr(os, 'name', 'nt')
+
+
+@pytest.mark.usefixtures('pretend_windows')
 class TestCase(BaseTestCase):
     def test_onlyunix(self):
         for command in ['chmod', 'chown', 'id', 'ln', 'login', 'mount', 'tee',
                         'uptime', 'whoami']:
-            os.name = 'nt'
             self.assertRaises(exception.CommandNotFoundException,
                               cmdix.getcommand, command)
 
