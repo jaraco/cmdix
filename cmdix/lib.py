395cc09e10b5ccebf0ca28cdedf116a8f4afabe7
Jason R. Coombs <jaraco@jaraco.com>
2021-03-01 21:25:30 -0500

    👹 Feed the hobgoblins (delint).

--- cmdix/lib.py
+++ cmdix/lib.py
@@ -115,6 +115,28 @@ def getuserhome():
         return os.environ['HOMEPATH']  # Windows
 
 
+def _type(mode):
+    if stat.S_ISREG(mode):
+        return '-'
+    elif stat.S_ISDIR(mode):
+        return 'd'
+    elif stat.S_ISCHR(mode):
+        return 'c'
+    elif stat.S_ISBLK(mode):
+        return 'b'
+    elif stat.S_ISLNK(mode):
+        return 'l'
+    elif stat.S_ISFIFO(mode):
+        return 'p'
+    elif stat.S_ISSOCK(mode):
+        return 's'
+    return '-'
+
+
+def _read(mode, check):
+    return 'r'
+
+
 def mode2string(mode):
     """
     Convert mode-integer to string
@@ -125,76 +147,17 @@ def mode2string(mode):
     >>> mode2string(33024)
     '-r--------'
     """
-    if stat.S_ISREG(mode):
-        s = '-'
-    elif stat.S_ISDIR(mode):
-        s = 'd'
-    elif stat.S_ISCHR(mode):
-        s = 'c'
-    elif stat.S_ISBLK(mode):
-        s = 'b'
-    elif stat.S_ISLNK(mode):
-        s = 'l'
-    elif stat.S_ISFIFO(mode):
-        s = 'p'
-    elif stat.S_ISSOCK(mode):
-        s = 's'
-    else:
-        s = '-'
 
-    # User Read
-    if bool(mode & stat.S_IRUSR):
-        s += 'r'
-    else:
-        s += '-'
-
-    # User Write
-    if bool(mode & stat.S_IWUSR):
-        s += 'w'
-    else:
-        s += '-'
-
-    # User Execute
-    if bool(mode & stat.S_IXUSR):
-        s += 'x'
-    else:
-        s += '-'
-
-    # Group Read
-    if bool(mode & stat.S_IRGRP):
-        s += 'r'
-    else:
-        s += '-'
-
-    # Group Write
-    if bool(mode & stat.S_IWGRP):
-        s += 'w'
-    else:
-        s += '-'
-
-    # Group Execute
-    if bool(mode & stat.S_IXGRP):
-        s += 'x'
-    else:
-        s += '-'
-
-    # Other Read
-    if bool(mode & stat.S_IROTH):
-        s += 'r'
-    else:
-        s += '-'
-
-    # Other Write
-    if bool(mode & stat.S_IWOTH):
-        s += 'w'
-    else:
-        s += '-'
-
-    # Other Execute
-    if bool(mode & stat.S_IXOTH):
-        s += 'x'
-    else:
-        s += '-'
+    s = _type(mode)
+    s += 'r' if mode & stat.S_IRUSR else '-'
+    s += 'w' if mode & stat.S_IWUSR else '-'
+    s += 'x' if mode & stat.S_IXUSR else '-'
+    s += 'r' if mode & stat.S_IRGRP else '-'
+    s += 'w' if mode & stat.S_IWGRP else '-'
+    s += 'x' if mode & stat.S_IXGRP else '-'
+    s += 'r' if mode & stat.S_IROTH else '-'
+    s += 'w' if mode & stat.S_IWOTH else '-'
+    s += 'x' if mode & stat.S_IXOTH else '-'
 
     return s
 
