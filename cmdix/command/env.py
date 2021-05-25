2068102379b3a39d8ced74303ba901848363b25e
Jason R. Coombs <jaraco@jaraco.com>
2021-05-25 10:42:02 -0400

    Remove broken code and add TODO to implement command execution.

--- cmdix/command/env.py
+++ cmdix/command/env.py
@@ -1,46 +1,24 @@
-import os
+# TODO: implement command execution.
 
-from .. import exception
+import os
 
 
 def parseargs(p):
-    """
-    Add arguments and `func` to `p`.
-
-    :param p: ArgumentParser
-    :return:  ArgumentParser
-    """
-    # TODO: --unset
     p.set_defaults(func=func)
     p.description = "Set each NAME to VALUE in the environment and run " + "COMMAND."
     p.usage = '%(prog)s [OPTION]... [-] [NAME=VALUE]... [COMMAND [ARG]...]'
-    p.add_argument('command')
     p.add_argument(
         "-i",
         "--ignore-environment",
-        action="store_true",
-        dest="ignoreenvironment",
+        action="store_const",
+        const=dict(),
+        dest="env",
         help="start with an empty environment",
+        default=os.environ,
     )
     return p
 
 
 def func(args):
-    env = {}
-    if not args.ignoreenvironment:
-        env = os.environ
-
-    if len(args) == 0:
-        for k, v in env.items():
-            print(k + '=' + v)
-    else:
-        for arg in args:
-            x = arg.split('=')
-            if len(x) < 2:
-                exception.StdErrException(
-                    "Invalid argument {0}. ".format(arg)
-                    + "Arguments should be in the form of 'foo=bar'",
-                    127,
-                )
-            else:
-                print(x[0] + '=' + x[1])
+    for k, v in args.env.items():
+        print(k + '=' + v)
