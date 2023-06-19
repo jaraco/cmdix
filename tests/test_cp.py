8bb9fe5350d71d4e173da7357d4ff81ab5d89367
Jason R. Coombs <jaraco@jaraco.com>
2023-06-19 17:14:56 -0400

    Add xfail test capturing missed expectation. Ref #13.

---
+++ tests/test_cp.py
@@ -0,0 +1,29 @@
+import os
+import types
+from unittest import mock
+
+import pytest
+
+from cmdix.command import cp
+
+
+@pytest.fixture
+def mocked_dirs(monkeypatch):
+    dirs = set()
+    real_isdir = os.path.isdir
+
+    def is_dir(cand):
+        return real_isdir(cand) or cand in dirs
+
+    monkeypatch.setattr(os.path, 'isdir', is_dir)
+
+    return dirs
+
+
+@pytest.mark.xfail(reason="logic is broken. Ref #13")
+def test_simple_cp_with_dest(mocked_dirs):
+    args = types.SimpleNamespace(verbose=False)
+    copy_fn = mock.Mock()
+    mocked_dirs.add('foo/bar')
+    cp.handle_direct(_copy=copy_fn, args=args, dest='foo/bar', src='/baz/bing.py')
+    copy_fn.assert_called_with('/baz/bing.py', 'foo/bar/bing.py')
