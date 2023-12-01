3dba3d8e0917c389ff42f316ea869c614806a3ce
Jason R. Coombs <jaraco@jaraco.com>
2023-11-30 21:26:34 -0500

    Implement support for env with subprocess. Closes #5

--- cmdix/command/env.py
+++ cmdix/command/env.py
@@ -1,6 +1,8 @@
-# TODO: implement command execution.
-
 import os
+import re
+import subprocess
+
+from more_itertools import partition
 
 
 def parseargs(p):
@@ -16,9 +18,50 @@ def parseargs(p):
         help="start with an empty environment",
         default=os.environ,
     )
+    p.add_argument("inputs", nargs="*")
     return p
 
 
 def func(args):
+    handler = invoke if args.inputs else print_env
+    return handler(args)
+
+
+def print_env(args):
     for k, v in args.env.items():
         print(k + '=' + v)
+
+
+class VarsCheck:
+    """
+    Partition inputs to env into vars and cmd args.
+
+    >>> vars, cmd = VarsCheck().partition(['foo=bar', 'bing=baz', 'blue', 'z=3'])
+    >>> dict(vars)
+    {'foo': 'bar', 'bing': 'baz'}
+    >>> list(cmd)
+    ['blue', 'z=3']
+    """
+
+    nonvar = False
+
+    def __call__(self, input):
+        return self.nonvar or self._check(input)
+
+    def _check(self, input):
+        self.nonvar = not re.match(r'\w+=.*$', input)
+        return self.nonvar
+
+    def partition(self, inputs):
+        vars, cmd = partition(self, inputs)
+        return map(_split, vars), cmd
+
+
+def _split(var):
+    return var.split('=', maxsplit=1)
+
+
+def invoke(args):
+    vars, cmd = VarsCheck().partition(args.inputs)
+    args.env.update(vars)
+    return subprocess.Popen(cmd, env=args.env).wait()
