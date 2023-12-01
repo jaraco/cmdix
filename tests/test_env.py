ccabe62128dda125b160ba394f35b335fdd3a1f2
Jason R. Coombs <jaraco@jaraco.com>
2023-11-30 21:39:20 -0500

    Update test for Windows compatibility

--- tests/test_env.py
+++ tests/test_env.py
@@ -3,8 +3,8 @@ from . import BaseTestCase
 
 class TestCase(BaseTestCase):
     def test_env(self, capfd):
-        self.runcommandline('env -i foo=bar env')
-        assert capfd.readouterr().out == 'foo=bar\n'
+        self.runcommandline('env -i FOO=bar env')
+        assert capfd.readouterr().out == 'FOO=bar\n'
 
     def test_env_simple(self, capfd):
         self.runcommandline('env')
