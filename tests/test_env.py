470971b684568ba37014549f60f24a708211a35b
Jason R. Coombs <jaraco@jaraco.com>
2023-11-30 21:35:29 -0500

    Add test for env with no parameters, completing coverage of env command.

--- tests/test_env.py
+++ tests/test_env.py
@@ -5,3 +5,7 @@ class TestCase(BaseTestCase):
     def test_env(self, capfd):
         self.runcommandline('env -i foo=bar env')
         assert capfd.readouterr().out == 'foo=bar\n'
+
+    def test_env_simple(self, capfd):
+        self.runcommandline('env')
+        assert capfd.readouterr().out
