46bf6b105a45151b1c20bcf8432ec3117b9495c0
Jason R. Coombs <jaraco@jaraco.com>
2025-02-12 11:19:45 -0500

    In test_rm_f, actually pass -f.
    Closes #21

--- tests/test_rm.py
+++ tests/test_rm.py
@@ -7,5 +7,5 @@ class TestCase(BaseTestCase):
     def test_rm_f(self, capsys):
         self.setup_filesystem()
         pathlib.Path('dir1/file1-1').chmod(0o400)
-        self.runcommandline('rm -r dir1')
+        self.runcommandline('rm -rf dir1')
         assert capsys.readouterr().out == ''
